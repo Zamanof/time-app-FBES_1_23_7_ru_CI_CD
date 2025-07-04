d7af02c225f20162e2725f7c4820df62558186fd
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-07-04 21:14:39 +0400

    fix unit test

--- api/__tests__/math.test.js
+++ api/__tests__/math.test.js
@@ -3,9 +3,9 @@ function division(a, b){
 }
 
 test('10 / 2 equal 5', ()=>{
-    expect(division(10, 0)).toBe(5)
+    expect(division(10, 2)).toBe(5)
 })
 
 test('15 / 3 equal 5', ()=>{
-    expect(division(14, 3)).toBe(5)
+    expect(division(15, 3)).toBe(5)
 })
